cafee9656a697e586b0cbf90fbf098be48b8cef5
Axil12 <alix.cdso@gmail.com>
2020-04-01 16:19:18 +0200

    Added 2 fire modes

--- IC06/Assets/Scripts/Shooting.cs
+++ IC06/Assets/Scripts/Shooting.cs
@@ -10,6 +10,9 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 5f;
+    public float shootDelay = 0.25f;
+    public bool canShoot = true;
+    public string weapon = "singleShot";
 
     Transform playerTransform;
 
@@ -31,8 +34,21 @@ public class Shooting : MonoBehaviour
 		}
     }
 
-    void Shoot()
-    {
+
+    public Vector3 getFirePointPosition(Vector3 playerPosition, float playerAngle) {
+        Vector3 firePoint = playerPosition;
+        float verticalFactor = 0.3f;
+        float horizontalFactor = 0.3f;
+        float verticalOffset = 0.0f;
+        float horizontalOffset = 0.0f;
+        firePoint = firePoint + new Vector3(Mathf.Cos(playerAngle)*horizontalFactor + horizontalOffset, Mathf.Sin(playerAngle)*verticalFactor + verticalOffset, 0);
+        return firePoint;
+	}
+
+    void setCanShootToTrue(){canShoot = true;}
+
+    void singleShot() {
+        Invoke("setCanShootToTrue", 0.25f);
         float shootingDirection = gameObject.GetComponent<PlayerMovement>().getAngle();
         GameObject bullet = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position, shootingDirection), Quaternion.Euler(0, 0, shootingDirection));
         bullet.GetComponent<Bullet>().setColor(playerColor);
@@ -41,13 +57,65 @@ public class Shooting : MonoBehaviour
         rb.AddForce(rb.transform.right * bulletForce, ForceMode2D.Impulse);
 	}
 
-    public Vector3 getFirePointPosition(Vector3 playerPosition, float playerAngle) {
-        Vector3 firePoint = playerPosition;
-        float verticalFactor = 0.0f;
-        float horizontalFactor = 0.5f;
-        float verticalOffset = 0.15f;
-        float horizontalOffset = 0.24f;
-        firePoint = firePoint + new Vector3(Mathf.Cos(playerAngle)*horizontalFactor + horizontalOffset, Mathf.Sin(playerAngle)*verticalFactor + verticalOffset, 0);
-        return firePoint;
+    void shotgunShot() {
+        Invoke("setCanShootToTrue", 0.33f);
+        float shootingDirection = gameObject.GetComponent<PlayerMovement>().getAngle();
+        GameObject bullet1 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position, shootingDirection), Quaternion.Euler(0, 0, shootingDirection));
+        GameObject bullet2 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position, shootingDirection + 10), Quaternion.Euler(0, 0, shootingDirection + 10));
+        GameObject bullet3 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position, shootingDirection - 10), Quaternion.Euler(0, 0, shootingDirection - 10));
+        /*
+        GameObject bullet2 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position + new Vector3(Mathf.Sin(shootingDirection*0.017453f)*0.5f,Mathf.Cos(shootingDirection*0.017453f)*0.5f,0), shootingDirection), Quaternion.Euler(0, 0, shootingDirection + 10));
+        GameObject bullet3 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position + new Vector3(Mathf.Sin(shootingDirection*0.017453f)*0.5f,-Mathf.Cos(shootingDirection*0.017453f)*0.5f,0), shootingDirection), Quaternion.Euler(0, 0, shootingDirection - 10));
+        */
+        bullet1.GetComponent<Bullet>().setColor(playerColor);
+        bullet2.GetComponent<Bullet>().setColor(playerColor);
+        bullet3.GetComponent<Bullet>().setColor(playerColor);
+        Rigidbody2D rb1 = bullet1.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
+        rb1.rotation = shootingDirection;
+        rb2.rotation = shootingDirection;
+        rb3.rotation = shootingDirection;
+        /*rb1.AddForce(rb1.transform.right * bulletForce, ForceMode2D.Impulse);
+        rb2.AddForce(rb2.transform.right * bulletForce, ForceMode2D.Impulse);
+        rb3.AddForce(rb3.transform.right * bulletForce, ForceMode2D.Impulse);*/
+	}
+
+    void tripleShot() {
+        Invoke("setCanShootToTrue", 0.4f);
+        float shootingDirection = gameObject.GetComponent<PlayerMovement>().getAngle();
+        GameObject bullet1 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position, shootingDirection), Quaternion.Euler(0, 0, shootingDirection));
+        GameObject bullet2 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position + new Vector3(Mathf.Cos(shootingDirection*0.017453f)*0.2f,Mathf.Sin(shootingDirection*0.017453f)*0.2f,0), shootingDirection), Quaternion.Euler(0, 0, shootingDirection));
+        GameObject bullet3 = Instantiate(bulletPrefab, getFirePointPosition(playerTransform.position + new Vector3(Mathf.Cos(shootingDirection*0.017453f)*0.4f,Mathf.Sin(shootingDirection*0.017453f)*0.4f,0), shootingDirection), Quaternion.Euler(0, 0, shootingDirection));
+        bullet1.GetComponent<Bullet>().setColor(playerColor);
+        bullet2.GetComponent<Bullet>().setColor(playerColor);
+        bullet3.GetComponent<Bullet>().setColor(playerColor);
+        Rigidbody2D rb1 = bullet1.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
+        rb1.rotation = shootingDirection;
+        rb2.rotation = shootingDirection;
+        rb3.rotation = shootingDirection;
+        rb1.AddForce(rb1.transform.right * bulletForce, ForceMode2D.Impulse);
+        rb2.AddForce(rb2.transform.right * bulletForce, ForceMode2D.Impulse);
+        rb3.AddForce(rb3.transform.right * bulletForce, ForceMode2D.Impulse);
+	}
+
+    void Shoot()
+    {
+        if (!canShoot)
+            return;
+        else
+            canShoot = false;
+
+        if (weapon == "shotgun") {
+            shotgunShot();
+		}
+        else if (weapon == "burst") {
+            tripleShot();  
+		}
+        else {
+            singleShot();  
+		}
 	}
 }
