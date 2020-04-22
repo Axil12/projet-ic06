b22612e5432e3b2781f5ca8eeb0ca56fb94f7ca3
Robin <robinrosseeuw.rr@gmail.com>
2020-04-22 19:10:06 +0200

    Menu V1 fonctionnel

---
+++ IC06/Assets/Scripts/Behaviours/Menu/MenuScript.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    // SINGLETON : Evite de transformer toutes les autres variables en variables statiques, vu que l'on renseignera cette Toolbox à la place
+    public static MenuScript instance = null;
+
+    // SONS
+    public enum Sons
+    {
+        PressStart = 0
+    }
+
+    // REFERENCES OBJETS
+    public GameObject logo = null;
+    public GameObject mainMenu = null;
+    public GameObject optionsMenu = null;
+
+    // REFERENCES COMPONENTS
+    public AudioSource menuAudioSource = null;
+    public PressStartScript scriptPressStart = null;
+
+    // PROPRIETES PUBLIQUES
+
+    // PROPRIETES PRIVATE
+    bool isStarted = false;
+
+    // METHODES
+    private void Awake()
+    {
+        // INSTANCIATION SINGLETON
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        // REFERENCES MANQUANTES
+        if (logo == null)
+        {
+            logo = this.transform.Find("TitleLogo").gameObject;
+        }
+
+        if (menuAudioSource == null)
+        {
+            menuAudioSource = this.GetComponent<AudioSource>();
+        }
+
+        if (mainMenu == null)
+        {
+            mainMenu = this.transform.Find("MainMenu").gameObject;
+        }
+
+        if (optionsMenu == null)
+        {
+            optionsMenu = this.transform.Find("OptionsMenu").gameObject;
+        }
+
+        if (scriptPressStart == null)
+        {
+            scriptPressStart = this.transform.Find("PressStart").GetComponent<PressStartScript>();
+        }
+
+        // ECRAN DE DEPART
+        InitAffichage();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Submit")){
+            // DETECTION START (Ecran Titre)
+            if (isStarted == false)
+            {
+                isStarted = true;
+                scriptPressStart.Accelerer();
+            }
+        }
+    }
+
+    // METHODES BOUTONS
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void EnterOptions()
+    {
+        // MAJ AFFICHAGE
+        logo.SetActive(false);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        optionsMenu.transform.Find("VolumeSlider").GetComponent<Slider>().Select();
+    }
+
+    public void LeaveOptions()
+    {
+        // MAJ AFFICHAGE
+        logo.SetActive(true);
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        mainMenu.transform.Find("PlayButton").GetComponent<Button>().Select();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting the game...");
+        if (Application.isEditor)
+        {
+            EditorApplication.ExecuteMenuItem("Edit/Play");
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    // METHODES GENERALES
+
+    private void InitAffichage()
+    {
+        logo.SetActive(true);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        scriptPressStart.gameObject.SetActive(true);
+        AudioListener.volume = 0.5f;
+    }
+
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void JouerSon(Sons SonAJouer)
+    {
+        string chemin = "Sounds/" + SonAJouer.ToString();
+        menuAudioSource.clip = Resources.Load(chemin, typeof(AudioClip)) as AudioClip;
+        menuAudioSource.Play();
+    }
+}
---
+++ IC06/Assets/Scripts/Behaviours/Menu/SliderScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using static UnityEngine.UI.Slider;
+
+public class SliderScript : MonoBehaviour
+{
+    // REFERENCES
+    public TextMeshProUGUI valeur;
+    private Slider sliderScript;
+
+    void Start()
+    {
+        // RECUP ELEMENT MANQUANT
+        if(valeur == null)
+        {
+            valeur = transform.Find("Value").GetComponent<TextMeshProUGUI>();
+        }
+
+        if (sliderScript == null)
+        {
+            sliderScript = this.GetComponent<Slider>();
+        }
+
+        // AJOUT EVENT
+        sliderScript.onValueChanged.AddListener(MiseAJour);
+    }
+
+    public void MiseAJour(float sliderValue)
+    {
+        valeur.text = (sliderValue * 100).ToString("N0") + "%";
+        AudioListener.volume = sliderValue;
+    }
+}
